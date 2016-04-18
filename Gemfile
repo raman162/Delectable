4c7f0ae8ac9e38446c9b9cded5dd06abda7cf06f
Raman Walwyn-Venugopal <ramanvenu94@gmail.com>
2016-04-17 23:41:08 -0500

    added coverage and a script to install all the required gems to run the app

--- Gemfile
+++ Gemfile
@@ -3,3 +3,4 @@ source 'https://rubygems.org'
 # Specify your gem's dependencies in delectable.gemspec
 gemspec
 gem 'simplecov', :require => false, :group => :test
+gem 'sinatra', :require => true
---
+++ rubyInstaller.sh
@@ -0,0 +1,11 @@
+ #!/bin/bash        
+
+ sudo apt-get install ruby
+ sudo apt-get install ruby-dev
+ sudo apt-get install g++
+ sudo gem install bundler
+ sudo gem install sinatra
+ sudo gem install thin
+ sudo gem install rspec
+ sudo gem install json
+ sudo gem install simplecov
